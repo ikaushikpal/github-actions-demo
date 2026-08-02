7429872feba24289f2770ffcbde7f1ab1529c5c2
Kaushik Pal <iamkaushik2014@m3.com>
2026-08-02 16:42:40 +0530

    feat: added subtract feature

--- test_main.py
+++ test_main.py
@@ -1,8 +1,13 @@
 """Tests for main.py."""
 
-from main import add
+from main import add, subtract
 
 
 def test_add():
     """Test addition."""
     assert add(10, 5) == 15, "Expected 10 + 5 to equal 15"
+
+
+def test_subtract():
+    """Test subtraction."""
+    assert subtract(10, 5) == 5, "Expected 10 - 5 to equal 5"
